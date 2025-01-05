67a8be31f08e1813dbddd6ef28b4ed0eac1b4db0
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-01-05 09:59:53 +0530

    Shifting Letters II

---
+++ Day59.py
@@ -0,0 +1,24 @@
+# Shifting Letters II
+
+class Solution:
+    def shiftingLetters(self, s, shifts):
+        N = len(s)
+
+        delta = [0] * (N + 1)
+        for left, right, direction in shifts:
+            if direction == 0:
+                direction = -1
+            
+            delta[left] += direction
+            delta[right + 1] -= direction 
+        
+        ans =[]
+        current_delta = 0
+        for i in range(N):
+            current = ord(s[i]) - ord('a')
+            current_delta += delta[i]
+            current += current_delta
+            current %= 26
+            
+            ans.append(chr(current + ord('a')))
+        return "".join(ans)
