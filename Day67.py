931e0b3b8e136ee672fd41c42d81df406fab46bf
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-01-13 18:53:24 +0530

    Minimum length of string After Operations

---
+++ Day67.py
@@ -0,0 +1,13 @@
+# Minimum length of string After Operations
+
+class Solution:
+    def minimumLength(self, s):
+        s_set = {'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans
