cdecc4460743e400e772cc98658f2ef496f15920
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-03-03 17:35:08 +0530

    Partition Array According to Given pivot

---
+++ Day116.py
@@ -0,0 +1,16 @@
+# Partition Array According to Given pivot
+
+class Solution:
+    def pivotArray(self, nums, pivot):
+        left =[]
+        middle = []
+        right =[]
+
+        for x in nums:
+            if x < pivot:
+                left.append(x)
+            elif x == pivot:
+                middle.append(x)
+            else:
+                right.append(x)
+        return left + middle + right
