9562a54959c5391e9e3b7de14a4c6756589e9b30
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-04-14 19:34:55 +0530

    Count Good Triplets

---
+++ Day154.py
@@ -0,0 +1,12 @@
+# Count Good Triplets
+
+class Solution:
+    def countGoodTriplets(self, arr, a, b, c):
+        N = len(arr)
+        count = 0
+        for i in range(N):
+            for j in range(i + 1, N):
+                for k in range(j + 1, N):
+                    if abs(arr[i] - arr[j])<= a and abs(arr[j]- arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        count += 1
+        return count
