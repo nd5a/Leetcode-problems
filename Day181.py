2ac16666303d79f6b671f7d6f20e486e3ecb9a6c
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-05-11 13:14:10 +0530

    Three Consecutive Odds

---
+++ Day181.py
@@ -0,0 +1,8 @@
+# Three Consecutive Odds
+
+class Solution:
+    def threeConsecutiveOdds(self, arr):
+        for i in range(len(arr) - 2):
+            if arr[i] % 2 == 1 and arr[i + 1] % 2==1 and arr[i+2]%2==1:
+                return True
+        return False
