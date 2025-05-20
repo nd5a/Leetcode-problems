90cab6a586d585a7afa91b83777f4073de5e1f50
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-05-20 19:47:39 +0530

    Zero Array Transformation I

---
+++ Day190.py
@@ -0,0 +1,17 @@
+# Zero Array Transformation I
+
+class Solution:
+    def isZeroArray(self, nums, queries):
+        N = len(nums)
+        delta = [0] * (N + 1)
+        for l, r in queries:
+            delta[l] += 1
+            delta[r + 1] -= 1
+        
+        for i in range(1, N + 1):
+            delta[i] += delta[i - 1]
+        
+        for i in range(N):
+            if nums[i] > delta[i]:
+                return False
+        return True
