1cae404170557f73c44c4d462607cc1877eb14b6
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-06-30 09:17:48 +0530

    Longest Harmonious Subsequence

---
+++ Day231.py
@@ -0,0 +1,13 @@
+# Longest Harmonious Subsequence
+
+import collections
+
+class Solution:
+    def findLHS(self, nums):
+        f = collections.Counter(nums)
+
+        best = 0
+        for x in f.keys():
+            if f[x] > 0 and f[x + 1] > 0:
+                best = max(best, f[x] + f[x + 1])
+        return best
