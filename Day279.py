9ca94d17b70fd3e920a4f0862ea118d8df809013
Dhruvil Nakrani <dhnakrani80@gmail.com>
2025-08-19 20:06:20 +0530

    Numbers of Zero-Filled Subarrays

---
+++ Day279.py
@@ -0,0 +1,16 @@
+# Numbers of Zero-Filled Subarrays
+
+class Solution:
+    def zeroFilledSubarray(self, nums):
+        N = len(nums)
+
+        streak = 0
+        total = 0
+
+        for x in nums:
+            if x == 0:
+                streak += 1
+                total += streak
+            else:
+                streak = 0
+        return total
